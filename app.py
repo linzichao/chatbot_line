8c7fd9d95585187b14ad33e23dc3dd22cf60f22a
linzichao <joy10393@gmail.com>
2018-04-19 01:05:34 +0800

    modify reply message

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ def ptt_hot():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message("Debug:" + event.reply_token,message)
+    line_bot_api.reply_message(event.reply_token, "Debug: " + message)
 
     if event.message.text == "熱門文章":
         content = ptt_hot()
