0c9dde0aa0c87d25497ad2c95b8857b812d83c56
linzichao <joy10393@gmail.com>
2018-04-21 20:37:11 +0800

    debug

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def ptt_hot():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
 
-    print(event)
+    print(event.source.userId)
 
     if event.message.text == "熱門文章":
         content = ptt_hot()
