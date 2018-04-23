75ddfb164181bac0af23752ba90952934e8809dd
linzichao <joy10393@gmail.com>
2018-04-23 13:23:56 +0800

    debug

--- app.py
+++ app.py
@@ -115,21 +115,21 @@ def handle_message(event):
                         ]
                     ),
                     CarouselColumn(
-                        title='資超的專案',
+                        title='額外功能',
                         text='請選擇',
-                        thumbnail_image_url='https://i.imgur.com/EuRHQUt.jpg',
+                        thumbnail_image_url='https://i.imgur.com/lgPvzHB.png',
                         actions=[
-                            URITemplateAction(
-                                label='Cigarette Smokers Problem',
-                                uri='https://github.com/linzichao/OS_smoker'
+                            MessageTemplateAction(
+                                label='PTT熱門文章',
+                                text='PTT熱門文章'
                             ),
-                            URITemplateAction(
-                                label='智慧型衛生紙',
-                                uri='https://goo.gl/co1CdZ'
+                            MessageTemplateAction(
+                                label='Dcard熱門文章',
+                                text='Dcard熱門文章'
                             ),
-                            URITemplateAction(
-                                label='Wake Up At Dawn',
-                                uri='https://linzichao.github.io/3D_final/'
+                            MessageTemplateAction(
+                                label='Youtube熱門影片',
+                                text='Youtube熱門影片'
                             )
                         ]
                     )
