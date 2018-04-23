11b71802381f052ade410ffb8263cc917329c33c
linzichao <joy10393@gmail.com>
2018-04-23 13:21:52 +0800

    debug

--- app.py
+++ app.py
@@ -133,7 +133,7 @@ def handle_message(event):
                             )
                         ]
                     )
-
+                ]
             )
         )
         line_bot_api.reply_message(event.reply_token, carousel_template)
