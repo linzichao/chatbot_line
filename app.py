10dbef88e47aa42c8fddb9eebd593380683d1a96
linzichao <joy10393@gmail.com>
2018-04-23 14:29:42 +0800

    debug

--- app.py
+++ app.py
@@ -113,6 +113,25 @@ def handle_message(event):
                                 text='Youtube熱門影片'
                             )
                         ]
+                    ),
+                    CarouselColumn(
+                        title='額外功能',
+                        text='請選擇',
+                        thumbnail_image_url='https://i.imgur.com/lgPvzHB.png',
+                        actions=[
+                            MessageTemplateAction(
+                                label='PTT熱門文章',
+                                text='PTT熱門文章'
+                            ),
+                            MessageTemplateAction(
+                                label='Dcard熱門文章',
+                                text='Dcard熱門文章'
+                            ),
+                            MessageTemplateAction(
+                                label='Youtube熱門影片',
+                                text='Youtube熱門影片'
+                            )
+                        ]
                     )
                 ]
             )
